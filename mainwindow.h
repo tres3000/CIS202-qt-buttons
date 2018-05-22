535a87515ab18ed20f71ed0d100129ded2369824
jared acosta <acostaj139344@student.vvc.edu>
2018-05-22 14:46:09 -0700

    buttons project

--- mainwindow.h
+++ mainwindow.h
@@ -4,7 +4,6 @@
 #include <QLabel>
 #include <QMainWindow>
 #include <QPushButton>
-#include <QSerialPort>
 
 class MainWindow : public QMainWindow
 {
@@ -13,22 +12,20 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
 
-private slots:
-    void openSerialPort();
-    void closeSerialPort();
-    void handleError(QSerialPort::SerialPortError error);
-    void readData();
-
 public slots:
     void onbutton1Press(bool toggled);
-
+    void onbutton2Press(bool toggled);
+    void onbutton3Press(bool toggled);
 
 private:
     QPushButton *button1;
     QLabel *label1;
-    QSerialPort *m_serial = nullptr;
 
-    void writeData(const char *data);
+    QPushButton *button2;
+    QLabel *label2;
+
+    QPushButton *button3;
+    QLabel *label3;
 };
 
 #endif // MAINWINDOW_H
